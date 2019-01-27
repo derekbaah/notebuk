53265a7508dd965a66bde0400d6d3702c19d7b52
derekbaah <derekbaah@gmail.com>
2019-01-27 00:16:00 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  resources :notes
+  get 'welcome/index'
+  
+  authenticated :user do
+    root 'notes#index'
+end
+
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
